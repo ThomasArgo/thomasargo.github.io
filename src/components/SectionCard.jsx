05b03c3b9f3be68621165d7a0ac90856698ee4c8
Thomas Argo <thomasargo182@gmail.com>
2026-04-27 09:49:59 -0500

    Delete src directory

--- src/components/SectionCard.jsx
+++
@@ -1,5 +0,0 @@
-export default function SectionCard({ children, className = "" }) {
-  const classes = ["surface-card", className].filter(Boolean).join(" ");
-
-  return <section className={classes}>{children}</section>;
-}
